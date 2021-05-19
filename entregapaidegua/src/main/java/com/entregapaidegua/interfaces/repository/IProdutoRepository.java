fc4a8922becb0335e038350f0a18fb1385bbbc2c
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-18 23:10:40 -0300

    interface de serviço de produtos

---
+++ entregapaidegua/src/main/java/com/entregapaidegua/interfaces/repository/IProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.entregapaidegua.interfaces.repository;
+
+
+
+import com.entregapaidegua.entity.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IProdutoRepository extends JpaRepository<Produto, Long> {
+    
+}

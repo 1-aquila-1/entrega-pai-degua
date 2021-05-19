f411cef7c30a2538314fbcd9c033cebc7efa51dc
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-18 23:10:30 -0300

    interface de serviço basico

---
+++ entregapaidegua/src/main/java/com/entregapaidegua/interfaces/service/IBaseService.java
@@ -0,0 +1,10 @@
+package com.entregapaidegua.interfaces.service;
+
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface IBaseService<T, ID> {
+    void cadastro(T o) throws Exception;
+    T obter(ID id) throws Exception;
+    T editar(ID id, T o) throws Exception;
+}

7300d527a10f51cb5f32e1249ff5863e0e6ef3c2
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-18 23:11:28 -0300

    produto objeto value

---
+++ entregapaidegua/src/main/java/com/entregapaidegua/vo/ProdutoVO.java
@@ -0,0 +1,15 @@
+package com.entregapaidegua.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProdutoVO {
+    private Long id;
+    private String nome;
+}

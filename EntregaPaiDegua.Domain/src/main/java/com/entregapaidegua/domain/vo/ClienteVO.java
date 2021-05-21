93b230c0c2bd3cfc9bc7c973731e5965f05464fb
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-20 23:01:23 -0300

    VO para o modelo cliente

---
+++ EntregaPaiDegua.Domain/src/main/java/com/entregapaidegua/domain/vo/ClienteVO.java
@@ -0,0 +1,49 @@
+package com.entregapaidegua.domain.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ClienteVO implements Serializable {
+
+    private Long id;
+    private String nome;
+    private Date dataNascimento;
+    private Integer telefone;
+    private EnderecoVO endereco;
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClienteVO other = (ClienteVO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+
+}

2ff163624a3a08c5402c2d192d728fe22501baf1
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-02 23:50:12 -0300

    Método cadastrar e editar alterado para chamada do serviço

--- entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/controller/EmpresaController.java
+++ entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/controller/EmpresaController.java
@@ -1,24 +1,39 @@
 package com.entregapaidegua.webapi.controller;
 
 import com.entregapaidegua.entity.Empresa;
+import com.entregapaidegua.interfaces.service.IEmpresaService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/empresa")
 public class EmpresaController {
-    
-    @PostMapping(produces = "application/json")
-    public ResponseEntity<Empresa> cadastrar(){
-        return ResponseEntity.ok(new Empresa());
+
+    public EmpresaController(IEmpresaService empresaService) {
+        this.empresaService = empresaService;
     }
 
-    @PutMapping(path = "/{/id}", produces = "application/json")
-    public ResponseEntity<Empresa> editar(){
-        return ResponseEntity.ok(new Empresa());
+    private IEmpresaService empresaService;
+    
+    @PostMapping(produces = "application/json")
+    public ResponseEntity<Empresa> cadastrar(@RequestBody Empresa empresa) throws Exception{
+        try{
+            empresaService.cadastrar(empresa);
+            return ResponseEntity.ok(empresa);
+        }catch(Exception ex){
+            throw new Exception(ex);
+        }
+    }
+
+    @PutMapping(path = "/{id}", produces = "application/json")
+    public ResponseEntity<Empresa> editar(@PathVariable Long id, @RequestBody Empresa empresa) throws Exception{
+        empresaService.editar(id, empresa);
+        return ResponseEntity.ok(empresa);
     }
 }

99de9382ecaec6e4c5d5f9fec254e2e273fc5e01
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-03 23:38:59 -0300

    Add anotação de packegeClasses, e o método ignoreParameterTypes -> Usuario

--- entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/config/SwaggerConfig.java
+++ entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/config/SwaggerConfig.java
@@ -1,30 +1,37 @@
 package com.entregapaidegua.webapi.config;
 
+
+import com.entregapaidegua.entity.Usuario;
+import com.entregapaidegua.webapi.controller.EmpresaController;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
-@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+    EmpresaController.class
+})
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build()
-          .apiInfo(metaInfo());
-    }
 
+    @Bean
+    public Docket forumApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .paths(PathSelectors.any())
+        .build()
+        .ignoredParameterTypes(Usuario.class)
+        .apiInfo(metaInfo());
+    }
+    
     private ApiInfo metaInfo() {
         return new ApiInfoBuilder()
         .title("Entrega Pai D'egua")
@@ -32,5 +39,5 @@ public class SwaggerConfig {
         .version("1.0")
         .contact(new Contact("Entrega Pai D'egua", "www.entregapaidegua.com", "")).build();
     }
-   
+
 }

1857ad7906c4764f3fa68c61626c634aefc9f7e1
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-03 23:32:46 -0300

    Adicionado a anotação @Service

--- entregapaidegua/src/main/java/com/entregapaidegua/interfaces/service/IEmpresaService.java
+++ entregapaidegua/src/main/java/com/entregapaidegua/interfaces/service/IEmpresaService.java
@@ -2,5 +2,8 @@ package com.entregapaidegua.interfaces.service;
 
 import com.entregapaidegua.entity.Empresa;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface IEmpresaService extends IBaseService<Empresa, Long>{
 }

9a7653138ed142f438a850d78106cb1507c776d6
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-06 22:53:41 -0300

    Adicionado a anotação JMap

--- entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/controller/dto/EmpresaDto.java
+++ entregapaidegua.webapi/src/main/java/com/entregapaidegua/webapi/controller/dto/EmpresaDto.java
@@ -2,6 +2,8 @@ package com.entregapaidegua.webapi.controller.dto;
 
 import java.io.Serializable;
 
+import com.googlecode.jmapper.annotations.JMap;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class EmpresaDto implements Serializable {
+    @JMap
     @ApiModelProperty(example = "Lider", position = 2)
     private String nome;
+    @JMap
     @ApiModelProperty(example = "12345678963", position = 3)
     private String documentoFiscal;
+    @JMap
     @ApiModelProperty(position = 4)
     private EnderecoDto endereco;
 }

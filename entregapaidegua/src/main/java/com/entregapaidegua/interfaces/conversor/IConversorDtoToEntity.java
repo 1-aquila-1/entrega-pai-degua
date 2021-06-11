6cec5e3bbfb231d9af19f6197542d27f55228d6a
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-10 23:14:27 -0300

    Exclusão de interfaces conversoras

--- entregapaidegua/src/main/java/com/entregapaidegua/interfaces/conversor/IConversorDtoToEntity.java
+++
@@ -1,8 +0,0 @@
-package com.entregapaidegua.interfaces.conversor;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IConversorDtoToEntity<Dto, Entity> {
-    Entity converterToEntity(Dto objeto);
-}
--- entregapaidegua/src/main/java/com/entregapaidegua/interfaces/conversor/IConversorEntityToDto.java
+++
@@ -1,8 +0,0 @@
-package com.entregapaidegua.interfaces.conversor;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IConversorEntityToDto<Entity, Dto> {
-    Dto converterToDto(Entity objeto);
-}

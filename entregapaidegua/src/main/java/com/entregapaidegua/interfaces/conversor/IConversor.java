e2ffb4979d165dc194abf28e88920d50fd5fe50a
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-11 23:19:16 -0300

    Renomeação de método

--- entregapaidegua/src/main/java/com/entregapaidegua/interfaces/conversor/IConversor.java
+++ entregapaidegua/src/main/java/com/entregapaidegua/interfaces/conversor/IConversor.java
@@ -3,7 +3,7 @@ package com.entregapaidegua.interfaces.conversor;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface IConversor<TEntidade, Dto> {
-    TEntidade dtoToEntidade(Dto objeto);
-    Dto entidadeToDto(TEntidade objeto);
+public interface IConversor<TEntidade, VM> {
+    TEntidade vmToEntidade(VM objeto);
+    VM entidadeToVM(TEntidade objeto);
 }

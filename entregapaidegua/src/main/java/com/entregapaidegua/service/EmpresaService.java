9582a8374a4230896af4e85d94fb84508f99ebfa
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-14 22:41:57 -0300

    Refatoração do método editar e a chamada do método atualizar da entidade

--- entregapaidegua/src/main/java/com/entregapaidegua/service/EmpresaService.java
+++ entregapaidegua/src/main/java/com/entregapaidegua/service/EmpresaService.java
@@ -4,6 +4,9 @@ import com.entregapaidegua.entity.Empresa;
 import com.entregapaidegua.interfaces.repository.IEmpresaRepository;
 import com.entregapaidegua.interfaces.service.IEmpresaService;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmpresaService extends BaseService<Empresa, Long> implements IEmpresaService{
 
     public EmpresaService(IEmpresaRepository empresaRepository) {
@@ -15,6 +18,11 @@ public class EmpresaService extends BaseService<Empresa, Long> implements IEmpre
 
     @Override
     public void editar(Long id, Empresa entity) throws Exception {
+        var obj = empresaRepository.findById(id);
+        if(!obj.isPresent()){
+            throw new Exception("Sem registro para atualizar");
+        }
+        empresaRepository.save(obj.get().atualizar(entity));
     }
     
 }

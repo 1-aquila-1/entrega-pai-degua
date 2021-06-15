7abe12e3cbd8e6239da4a6780a737a418e5d178b
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-14 22:38:27 -0300

    Criado o método atualizar

--- entregapaidegua/src/main/java/com/entregapaidegua/entity/Empresa.java
+++ entregapaidegua/src/main/java/com/entregapaidegua/entity/Empresa.java
@@ -27,28 +27,10 @@ public class Empresa extends Entidade{
     @Embedded   
     private Endereco endereco;
     
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Empresa other = (Empresa) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+   public Empresa atualizar(Empresa obj){
+       setNome(obj.getNome());
+       setEndereco(obj.getEndereco());
+       return this;
+   }
     
 }

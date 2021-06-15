2ec9f8d4b9030858f188903edfb3fe02c726863d
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-06-14 22:42:52 -0300

    Finalização do método editar

--- entregapaidegua/src/main/java/com/entregapaidegua/service/ProdutoService.java
+++ entregapaidegua/src/main/java/com/entregapaidegua/service/ProdutoService.java
@@ -1,42 +1,27 @@
 package com.entregapaidegua.service;
 
-import java.util.Optional;
-
 import com.entregapaidegua.entity.Produto;
 import com.entregapaidegua.interfaces.repository.IProdutoRepository;
 import com.entregapaidegua.interfaces.service.IProdutoService;
-import com.entregapaidegua.vo.ProdutoVO;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProdutoService implements IProdutoService{
+public class ProdutoService extends BaseService<Produto, Long> implements IProdutoService{
+
+    public ProdutoService(IProdutoRepository repositorio) {
+        super(repositorio);
+        this.produtoRepo = repositorio;
+    }
 
     private IProdutoRepository produtoRepo;
 
-    @Autowired
-    public ProdutoService(IProdutoRepository produtoRepo) {
-        this.produtoRepo = produtoRepo;
-    }
-
     @Override
-    public void cadastro(ProdutoVO o) throws Exception {
-    }
-
-    @Override
-    public ProdutoVO obter(Long id) throws Exception {
-        Optional<Produto> obj = produtoRepo.findById(id);
-        if(!obj.isPresent()){
-
+    public void editar(Long id, Produto entity) throws Exception {
+        var prod = produtoRepo.findById(id);
+        if(!prod.isPresent()){
+            throw new Exception("Produdo não encontrado para atualização");
         }
-        var produto = new ProdutoVO(1L, "Teste");
-        return produto;
+        produtoRepo.save(prod.get().atualizar(entity));
     }
-
-    @Override
-    public ProdutoVO editar(Long id, ProdutoVO o) throws Exception {
-        return null;
-    }
-    
 }
